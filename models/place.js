8db8b5bfe8ba05907f68a86e3e22a0dc0c4025b3
VictorPena23 <halo_rider200219@yahoo.com>
2023-04-23 12:48:57 -0700

    adding address and location strings

--- models/place.js
+++ models/place.js
@@ -5,4 +5,10 @@ const placeSchema = new Schema({
     title: {type: String, required:true },
     description: { type: String, required:true },
     image: { type: String, required:true },
+    address: { type: String, required:true },
+    location: {
+        lat: { type: Number, required:true },
+        lng: { type: Number, required:true }
+    },
+    
 })
