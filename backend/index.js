827424b3f2de5e0b085a0da58ef74cb317751379
Tara Maddox <TMaddox85@gmail.com>
2023-04-23 12:44:14 -0700

    set middleware

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 require('dotenv').config
-
 const express = require('express')
-
+//Runs Express
 const app = express()
 
+//Route
 app.get('/', (req,res) => {
     res.json({mssg: 'Welcome to the app'})
 });
+
+//Middleware
+app.use(() => {
+    
+})
+
+//Listens To Port
 app.listen(3000,() => {
     console.log('listening on PORT 3000')
 })
