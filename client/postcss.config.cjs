7c1338352d9c26ee9778070d5aaa76cb0f2bae94
Karlie Griffin <113546703+KGriffin12@users.noreply.github.com>
2023-04-23 14:28:33 -0500

    css layout.

--- client/postcss.config.cjs
+++ client/postcss.config.cjs
@@ -1,3 +1,10 @@
 // transforms your CSS code into an abstract syntax tree (AST) and then 
 //provides an API (application programming interface) for analyzing and 
 //modifying it using JavaScript plugins
+
+module.exports = {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  }
