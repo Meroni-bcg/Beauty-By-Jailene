ff012a5adad51035827d048bc1d03a3323ddde79
Karlie Griffin <113546703+KGriffin12@users.noreply.github.com>
2023-04-23 14:56:04 -0500

    fixed postcss.

--- client/postcss.config.cjs
+++ client/postcss.config.cjs
@@ -1,14 +1,8 @@
 // transforms your CSS code into an abstract syntax tree (AST) and then 
 //provides an API (application programming interface) for analyzing and 
-<<<<<<< HEAD
-//modifying it using JavaScript plugins
-=======
-//modifying it using JavaScript plugins
-
 module.exports = {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   }
->>>>>>> 7c1338352d9c26ee9778070d5aaa76cb0f2bae94
