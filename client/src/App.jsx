d2c7bb12a1f4c68b1083797d83a6df9a104bbd5d
Karlie Griffin <113546703+KGriffin12@users.noreply.github.com>
2023-04-23 20:40:12 -0500

    Working on tab bar.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,18 +8,21 @@ function App() {
 
   return (
    <div>
-   <header>
+   <header className="p-4 flex justify-between gap-5">
     <a href= "" className='flex items-center gap-1'>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 -90 rotate">
   <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
 </svg>
-<span>Beauty by Jailene</span>
+<span className='font-bold text-xl'>Beauty by Jailene</span>
     </a>
-    <div className='flex'>
-      <div>Book</div>
-      <div>Location</div>
-      <div>Contact</div>
-      <div>Website</div>
+    <div className='flex border border-gray-300 rounded-full py-2 px-4'>
+      <div>Book </div>
+      <div className="border-l border-gray-300"></div>
+      <div>Location </div>
+      <div className="border-l border-gray-300"></div>
+      <div>Contact </div>
+      <div className="border-l border-gray-300"></div>
+      <div>Website </div>
       <button>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 15.75l-2.489-2.489m0 0a3.375 3.375 0 10-4.773-4.773 3.375 3.375 0 004.774 4.774zM21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
