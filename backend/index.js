dd1b44e2131317d9f0ae230a10e37487d27b99a7
Tara Maddox <TMaddox85@gmail.com>
2023-04-23 20:01:09 -0700

    input booking schema, time isn't finished

--- backend/index.js
+++ backend/index.js
@@ -6,20 +6,31 @@ const mongoose = require('mongoose')
 //Runs Express
 const app = express()
 
-//Route
-app.get('/', (req,res) => {
-    res.json({mssg: 'Welcome to the app'})
-});
-
 //Middleware
 app.use(() => {
     console.log(req.path, req.method)
     next()
 })
 
-//Listens To Port
-app.listen(3000,() => {
-    console.log('listening on PORT 3000')
+//routes
+app.get('/',(req,res) => {
+    res.json({message: 'Welcome to the app'})
 })
 
+//connect to db
+mongoose.connect((process.env.MONGO_URI))
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db and listening on PORT', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
 process.env
+
+/*Listens To Port
+app.listen(3000,() => {
+    console.log('listening on PORT 3000')
+})*/
