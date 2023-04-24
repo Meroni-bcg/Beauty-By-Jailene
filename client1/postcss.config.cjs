3a1792b10c5091325ff4d3e1dc7eb571bdbaae9a
Karlie Griffin <113546703+KGriffin12@users.noreply.github.com>
2023-04-23 20:10:39 -0500

    added yarn dependencies.

--- client1/postcss.config.cjs
+++
@@ -1,8 +0,0 @@
-// transforms your CSS code into an abstract syntax tree (AST) and then 
-//provides an API (application programming interface) for analyzing and 
-module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  }
