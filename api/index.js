5989202f6536bf98116759e85b9e2a68c84dfca8
Tara Maddox <TMaddox85@gmail.com>
2023-04-25 23:46:05 -0700

    made image folder

--- api/index.js
+++ api/index.js
@@ -4,20 +4,43 @@ const app = express();
 
 //app.use
 
+const express = require('express')
 
 
-mongoose.connect('');
 
 
+const router = express.Router()
+
+//GET all 
+
+
+//GET single 
+router.get('/:id',)
+
+//POST 
+router.post('/',)
+
+//DELETE 
+router.delete('/:id',)
+
+//UPDATE 
+router.patch('/:id',)
+
+module.exports = router
+
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db and listening on PORT', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
 //app.get
-
-
-
-
 app.get('/test', (req,res) =>{
 res.json('test ok');
-
 });
 
-app.listen(3000);
