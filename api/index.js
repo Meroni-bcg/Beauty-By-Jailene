7a95e2f72c6ef813e26e068b4219eb2e2b0ada93
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2023-04-26 22:02:00 -0700

    Tara committed changes to the Meroni Branch

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const app = express();
 
 //app.use
 
+<<<<<<< Updated upstream
 
 
 mongoose.connect('');
@@ -21,3 +22,45 @@ res.json('test ok');
 });
 
 app.listen(3000);
+=======
+const express = require('express')
+
+
+
+
+const router = express.Router()
+
+//GET all 
+
+
+//GET single 
+router.get('/:id',)
+
+//POST 
+router.post('/',)
+
+//DELETE 
+router.delete('/:id',)
+
+//UPDATE 
+router.patch('/:id',)
+
+module.exports = router
+
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db and listening on PORT', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+//app.get
+app.get('/test', (req,res) =>{
+res.json('test ok');
+});
+
+>>>>>>> Stashed changes
